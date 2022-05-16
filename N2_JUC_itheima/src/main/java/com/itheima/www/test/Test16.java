36468fa30ec56f92db6d7a8587bacad2305dcfa7
liming <ccclane@foxmail.com>
2022-05-16 21:42:17 +0800

    Update-itheima-JUC-Format

--- N2_JUC_itheima/src/main/java/com/itheima/www/test/Test16.java
+++ N2_JUC_itheima/src/main/java/com/itheima/www/test/Test16.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import static com.itheima.www.n2.util.Sleeper.sleep;
 
 /**
- *  应用之统筹（烧水泡茶）
+ * 应用之统筹（烧水泡茶）
  */
 @Slf4j(topic = "c.Test16")
 public class Test16 {
@@ -16,7 +16,7 @@ public class Test16 {
             sleep(1);
             log.debug("烧开水");
             sleep(5);
-        },"老王");
+        }, "老王");
 
         Thread t2 = new Thread(() -> {
             log.debug("洗茶壶");
@@ -31,7 +31,7 @@ public class Test16 {
                 e.printStackTrace();
             }
             log.debug("泡茶");
-        },"小王");
+        }, "小王");
 
         t1.start();
         t2.start();
