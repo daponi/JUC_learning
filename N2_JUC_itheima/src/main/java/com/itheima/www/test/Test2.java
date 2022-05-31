7570a60e968b520a540226189b0556e929269b28
liming <ccclane@foxmail.com>
2022-05-31 23:41:12 +0800

    Update-itheima-JUC-format

--- N2_JUC_itheima/src/main/java/com/itheima/www/test/Test2.java
+++ N2_JUC_itheima/src/main/java/com/itheima/www/test/Test2.java
@@ -8,7 +8,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j(topic = "c.Test2")
 public class Test2 {
     public static void main(String[] args) {
-        Runnable r = () -> {log.debug("running");};
+        Runnable r = () -> {
+            log.debug("running");
+        };
 
         Thread t = new Thread(r, "t2");
 
